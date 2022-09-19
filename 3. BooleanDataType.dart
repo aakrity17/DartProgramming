2ff110458970a29a08f92e5a30ba3d006dad77a5
Aakrity Chapagai <63911598+aakrity17@users.noreply.github.com>
2022-09-19 22:26:35 +0545

    Add files via upload

---
+++ 3. BooleanDataType.dart
@@ -0,0 +1,12 @@
+// Boolean values represents true and false. The keyword bool is used to represent a Boolean literal in DART
+
+void main() {
+  String str = "I am";
+  String str2 = "I am";
+  String str1 = "having fun learning Dart";
+  bool val = (str == str1); // Str and str1 is not same so it will print false
+  bool val1 = (str == str2); // Str and str2 is same so it will print true
+
+  print(val);
+  print(val1);
+}
