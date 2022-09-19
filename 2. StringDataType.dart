1438e19667a962f2eaf868ffefab41d1acf4883f
Aakrity Chapagai <63911598+aakrity17@users.noreply.github.com>
2022-09-19 22:02:59 +0545

    Add files via upload

---
+++ 2. StringDataType.dart
@@ -0,0 +1,10 @@
+// String: It is used to represent a sequence of characters
+//  The keyword string is used to represent String literals. String values are embedded in eiter signle or double quotes.
+
+void main() {
+  String string = "Aakrity"; //double quote
+  String str = 'World'; // Single quote
+  String str1 = "Fun";
+  print(string);
+  print(str + str1);
+}
