bbcb3d439696ec7c805e96d7a0ceac5a9c42c9bb
Aakrity Chapagai <63911598+aakrity17@users.noreply.github.com>
2022-09-20 18:00:21 +0545

    Map concepts
    Note − A map value can be any object including NULL.

---
+++ 5, Map.dart
@@ -0,0 +1,12 @@
+// The map object is a key and value pair. Keys and values on a map may be of anytype. It is a dynamic collection
+// Note: If the type of a variable is not specified, the variable’s type is dynamic.
+//The dynamic keyword is used as a type annotation explicitly.
+
+void main() {
+  Map cwk = new Map();
+  cwk['First'] = "Hello";
+  cwk['Second'] = "Aakrity";
+  cwk['Third'] = "Chapagai";
+  // cwk ['First']= "Hello";
+  print(cwk);
+}
