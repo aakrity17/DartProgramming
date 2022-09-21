79325dc1a49d57e2868bc320d820e0c9179f9dd3
Aakrity Chapagai <63911598+aakrity17@users.noreply.github.com>
2022-09-21 22:50:35 +0545

    Add files via upload

---
+++ 16.NestedSwitchCase.dart
@@ -0,0 +1,25 @@
+void main() {
+  int cwk1 = 1;
+  String cwk2 = "Hello";
+  switch (cwk1) {
+    case 1:
+      {
+        switch (cwk2) {
+          case 'Hello':
+            {
+              print("Welcome to my Github");
+            }
+        }
+      }
+      break;
+    case 2:
+      {
+        print("Number 2");
+      }
+      break;
+    default:
+      {
+        print("This is default");
+      }
+  }
+}
